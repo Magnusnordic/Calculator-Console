465ce2fe99e4f0a4ea5059708aa524473effcf78
Magnus Tillquist Nielsen <Magnusn750@gmail.com>
2022-06-09 10:42:33 +0200

    Create "num3" & update "Plus"

--- Program.cs
+++ Program.cs
@@ -17,13 +17,28 @@ namespace Calculator_Console
             {
                 case "1": //Plus
                     Console.Clear();
-                    Console.WriteLine("choice = Plus \n");
+                    Console.WriteLine("choice = Add");
+                    Console.WriteLine("------------------------\n");
+
                     Console.WriteLine("Type a number, and then press Enter");
                     int num1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Type another number, and then press Enter");
                     int num2 = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    //mere +
+                    Console.WriteLine("\n" + "More Add? \n [1] = Yes \n [2] = No");
+                    Console.Write("You choice? ");
+                    int add1 = Convert.ToInt32(Console.ReadLine());
+                    if(add1 == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You number " + $"{num1} + {num2}" + "\n");
+                        Console.WriteLine("Type 3th number, and then press Enter");
+                        int num3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Your result: {num1} + {num2} + {num3} = " + (num1 + num2 + { num3}));
+                    } else if(add1 == 2)
+                    {
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    }
                     break;
             }
         }
