128825a4e9cfc11b6bd982f83f115b6dccda51c1
Magnus Tillquist Nielsen <Magnusn750@gmail.com>
2022-06-09 10:20:29 +0200

    Update "Switch"

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace Calculator_Console
     {
         static void Main(string[] args)
         {
+            //Welcomme Message
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
 
+            //Switch
+            Console.WriteLine("Pick you choice\n" + "[1] = Plus" + "\n");
+            Console.Write("You choice? ");
             switch (Console.ReadLine())
             {
 
