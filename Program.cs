c84f10b601a975c43ebbc7a9a9bdb5588dac6b2e
Magnus Tillquist Nielsen <Magnusn750@gmail.com>
2022-06-09 10:24:53 +0200

    update "switch\plus>

--- Program.cs
+++ Program.cs
@@ -16,10 +16,13 @@ namespace Calculator_Console
             switch (Console.ReadLine())
             {
                 case "1": //Plus
+                    Console.Clear();
+                    Console.WriteLine("choice = Plus \n");
                     Console.WriteLine("Type a number, and then press Enter");
                     int num1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Type another number, and then press Enter");
                     int num2 = Convert.ToInt32(Console.ReadLine());
+
                     Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
                     break;
             }
